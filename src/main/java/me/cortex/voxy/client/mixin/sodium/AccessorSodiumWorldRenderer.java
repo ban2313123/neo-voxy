c4c73572a11370d4749edbd749a5c8d8bbe8bb16
ban2313123 <128211950+ban2313123@users.noreply.github.com>
2026-07-03 11:47:27 +1000

    Update AccessorSodiumWorldRenderer.java

--- src/main/java/me/cortex/voxy/client/mixin/sodium/AccessorSodiumWorldRenderer.java
+++ src/main/java/me/cortex/voxy/client/mixin/sodium/AccessorSodiumWorldRenderer.java
@@ -1,14 +1,15 @@
 package me.cortex.voxy.client.mixin.sodium;
 
-import it.unimi.dsi.fastutil.longs.Long2IntOpenHashMap;
 import net.caffeinemc.mods.sodium.client.render.SodiumWorldRenderer;
 import net.caffeinemc.mods.sodium.client.render.chunk.RenderSectionManager;
-import net.caffeinemc.mods.sodium.client.render.chunk.map.ChunkTracker;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(value = SodiumWorldRenderer.class, remap = false)
 public interface AccessorSodiumWorldRenderer {
-    @Accessor
+
+    @Accessor(remap = false)
     RenderSectionManager getRenderSectionManager();
+
+    // Если в 0.8 есть другие нужные поля — добавим позже
 }
